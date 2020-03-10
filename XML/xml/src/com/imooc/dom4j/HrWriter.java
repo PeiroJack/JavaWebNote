a89b68eaf0c6501865f871f916ae2baf4b2a9ad8
PeiroJack <3532926353@qq.com>
2020-03-11 00:08:28 +0800

    learn XML

--- XML/xml/src/com/imooc/dom4j/HrWriter.java
+++ XML/xml/src/com/imooc/dom4j/HrWriter.java
@@ -1,5 +1,6 @@
 package com.imooc.dom4j;
 
+
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -10,33 +11,31 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 public class HrWriter {
-	public void writeXml() {
-		String file = "D:/JavaWebNote/XML/xml/src/hr.xml";
-		SAXReader reader = new SAXReader();
+	public void XMLWriter() {
+		String file="D:/JavaWebNote/XML/xml/src/hr.xml";
+		SAXReader reader=new SAXReader();
 		try {
 			Document document = reader.read(file);
-			Element root=document.getRootElement();
-			Element employee=root.addElement("employee");
-			employee.addAttribute("no", "3011");
-			Element name=employee.addElement("name");
-			name.setText("李铁柱");
-			employee.addElement("age").setText("30");
-			employee.addElement("salary").setText("3600");
+			Element root = document.getRootElement();
+			Element employee = root.addElement("employee");
+			employee.addAttribute("no", "3305");
+			employee.addElement("name").setText("王铁柱");
+			employee.addElement("age").setText("56");
+			employee.addElement("salary").setText("8000");
 			Element department = employee.addElement("department");
-			department.addElement("dname").setText("人力资源部");
-			department.addElement("address").setText("海口村");
-			Writer writer = new OutputStreamWriter(new FileOutputStream(file),"UTF-8");
+			department.addElement("dname").addText("人力资源部");
+			department.addElement("address").addText("XX大夏-980");
+			Writer writer =new OutputStreamWriter(new FileOutputStream(file),"UTF-8");
 			document.write(writer);
 			writer.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public static void main(String[] args) {
-		HrWriter hrwriter = new HrWriter();
-		hrwriter.writeXml();
+		HrWriter hrWriter = new HrWriter();
+		hrWriter.XMLWriter();
 	}
 }
---
+++ XMLNote.md
@@ -0,0 +1,12 @@
+# XPath路径表达式
+
+XPath路径表达式是XML文档中查找数据的语言。
+
+
+
+Jaxen是一个Java编写的开源的XPath库。这是适应多种不同的对象模型，包括DOM，XOM，dom4j和JDOM
+
+Dom4j底层依赖Jaxen实现Xpath查询
+
+
+
